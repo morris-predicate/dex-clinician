21055c30ab21827f8de95f7788a2232813e191b0
Morris Nguyen <morris.nguyen@predicatelabs.ai>
2026-07-26 09:11:04 -0400

    fix: render clinician OAuth callback

--- AuthCallback.jsx
+++ AuthCallback.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { completeClinicianAuthentication } from "./clinicianCognito.js";
 
 export default function AuthCallback({ onAuthenticated }) {
